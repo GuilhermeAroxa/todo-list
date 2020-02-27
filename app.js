257fe86dc21bafe3270071ab1088588ceb317f52
GuilhermeAroxa <guilherme.aroxa@gmail.com>
2020-02-27 20:46:22 -0300

    post method checklist

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use('/checklists', checkListRouter);
 app.use('/', rootRouter);
 app.use(express.static(path.join(__dirname, 'public')))
--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -6,28 +6,38 @@ const Checklist = require('../models/checklist');
 router.get('/', (async (req, res) => {
     try{
         let checklists = await Checklist.find({});
-        res.status(200).json(checklists);
+        res.status(200).render('checklists/index', {checklists : checklists});
     } catch (error){
-        res.status(422).json(error);
+        res.status(422).render('pages/error', {error : 'Erro ao exibir as listas'});
+    }
+}))
+
+router.get('/new', (async (req, res) => {
+    try{
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', {checklist : checklist});
+    } catch (error){
+        res.status(422).render('pages/error', {error : 'Erro ao carregar o formulário'});
     }
 }))
 
 router.get('/:id', (async (req, res) => {
     try{
-        let checklists = await Checklist.findById(req.params.id);
-        res.status(200).json(checklists);
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/show', {checklist : checklist});
     } catch (error){
-        res.status(404).json(error);
+        res.status(422).render('pages/error', {error : 'Erro ao exibir as listas de tarefa'});
     }
 }))
 
 router.post('/', (async (req, res) => {
+    let { name } = req.body.checklist;
+    let checklist = new Checklist({name});
     try{
-        let { name } = req.body;
-        let checklist = await Checklist.create({ name })
-        res.status(201).json(checklist);
+        await checklist.save(checklist);
+        res.redirect('/checklists')
     } catch(error){
-        res.status(422).json(error);
+        res.status(422).render('checklists/new', {checklist : {...checklist, error}});
     }
 }))
 
